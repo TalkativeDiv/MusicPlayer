ba930b4edab7a646dfd98e96a9f956e783c7b3ab
TalkitiveDiv <abhipieznomnomnom@gmail.com>
2021-11-13 17:38:56 -0500

    🚀 first commit

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+const audio = new Audio();
+
+let playSong = (song) => {
+  audio.src = song;
+  audio.play();
+}
+
+
+// Preloader 
+window.addEventListener("load", function () {
+     const preloader = document.querySelector(".preloader");
+     preloader.className += " hidden"; // class "loader hidden"
+ });
